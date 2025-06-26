bf50aa820be1cc8db6e7a3c85028574a11ed553a
Bikesh Shrestha <bikeshrestha048+github@gmail.com>
2025-06-26 19:11:20 +0545

    Wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,31 @@
+local wezterm = require("wezterm")
+local mux = wezterm.mux
+
+local config = wezterm.config_builder()
+
+config.font_size = 18
+config.automatically_reload_config = true
+config.enable_tab_bar = false
+config.window_close_confirmation = "NeverPrompt"
+config.window_decorations = "RESIZE"
+config.default_cursor_style = "BlinkingBar"
+config.window_background_opacity = 0.7
+config.macos_window_background_blur = 7
+config.keys = {
+	{
+		key = "Enter",
+		mods = "ALT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+	{
+		key = "Enter",
+		mods = "CMD",
+		action = wezterm.action.ToggleFullScreen,
+	},
+	{
+		key = "s",
+		mods = "CMD",
+		action = wezterm.action.SendKey({ key = "s", mods = "CTRL" }),
+	},
+}
+return config
